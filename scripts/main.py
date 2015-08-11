befead38d54e718dd43b983d31b94ab6abb12fd2
Jim Maddock <klogg@Jims-MacBook-Pro.local>
2015-08-11 15:00:28 +0530

    first commit!

---
+++ scripts/main.py
@@ -0,0 +1,104 @@
+from pymongo import MongoClient
+from pandas import DataFrame
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#import ml_config
+import pickle
+import rumor_terms
+import re
+import numpy
+
+# db info
+client = MongoClient('z')
+
+#vectorizor info
+analyzer = 'word'
+ngram_range = (1,1)
+stopwords = 'english'
+tfidf = False
+
+def remove_stopwords(words,rumor,event):
+    stop_words = rumor_terms.filter_words[rumor] + rumor_terms.event_terms[event]
+    filtered_words = [re.sub("'","",w.lower()) for w in words if not re.sub("'","",w.lower()) in stop_words]
+    return filtered_words
+
+def scrub_tweet(text,scrub_url=True):
+        temp = None
+        s = ur'\u201c' + '@.*?:'
+        while text is not temp:
+            temp = text
+            text = re.sub('RT .*?:','',text).strip()
+            text = re.sub('"@.*?:','',text).strip()
+            text = re.sub(s,'',text).strip()
+            text = re.sub('via @.*?:','',text).strip()
+            text = re.sub('via @.*?\b','',text).strip()
+            text = re.sub('@.*?\b','',text).strip()
+            if scrub_url is True:
+                text = re.sub('http.*?\s|http.*?$','',text).strip()
+        #print text
+        return text
+
+def import_data(fname=None):
+    result = DataFrame({'text':[],'class':[]})
+    for event in rumor_terms.event_rumor_map:
+        for rumor in rumor_terms.event_rumor_map[event]:
+            rows = []
+            index = []
+            tweets = client[event][rumor].find({'codes.first_code':{'$in':['Affirm','Deny','Neutral']}})
+            for tweet in tweets:
+                text = scrub_tweet(tweet['text'])
+                text = remove_stopewords(text,rumor,event)
+                if "Uncertainty" in tweet['codes']['second_code']:
+                    classification = 1
+                else:
+                    classification = 0
+                rows.append({'text':text,'class':classification})
+                index.append(rumor)
+            data = DataFrame(rows,index=index)
+            result.append(data)
+            result = result.reindex(numpy.random.permutation(data.index))
+        break
+    if fname:
+        fpath = os.path.join(os.path.dirname(__file__),os.pardir,'dicts/') + fname
+        f = open(fpath, 'w')
+        pickle.dump(result,f)
+    return result
+
+def make_feature_set(labled_data,fname=None,unpickle=False,verbose=False):
+    if unpickle:
+        labled_data = pickle.load(labled_data)
+    if tfidf:
+        count_vectorizer = TfidfVectorizer(analyzer=analyzer,
+                                           ngram_range=ngram_range,
+                                           stopwords=stopwords)
+    else:
+        count_vectorizer = CountVectorizer(analyzer=analyzer,
+                                           ngram_range=ngram_range,
+                                           stopwords=stopwords)
+    counts = count_vectorizer.fit_transform(labeled_data['text'].values)
+    if fname:
+        fpath = os.path.join(os.path.dirname(__file__),os.pardir,'dicts/') + fname
+        f = open(fpath, 'w')
+        pickle.dump(result,f)
+    if verbose:
+        print count_vectorizer.vocabulary
+    return counts
+
+'''
+def make_pipeline(labled_data):
+    pipeline = Pipeline([
+        ('vectorizer',  CountVectorizer()),
+        ('classifier',  MultinomialNB()) ])
+
+    pipeline.fit(data['text'].values, data['class'].values
+                 pipeline.predict(examples) # ['spam', 'ham']
+'''
+
+def main():
+    documents = import_data()
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/rumor_terms.py
@@ -0,0 +1,341 @@
+import re
+
+rumor_terms = {
+    ### DC power outage Rumors ##
+    'explosion':{
+        '$or':[
+            {'text':re.compile('explosion',re.IGNORECASE)},
+            {'text':re.compile('blast',re.IGNORECASE)},
+            {'text':re.compile('boom',re.IGNORECASE)}
+        ]
+    },
+    'foul_play':{
+        '$or':[
+            {'text':re.compile('foul',re.IGNORECASE)},
+            {'text':re.compile('terror',re.IGNORECASE)},
+            {'text':re.compile('attack',re.IGNORECASE)},
+            {'text':re.compile('hack',re.IGNORECASE)}
+        ]
+    },
+    ### WestJet Rumors ##
+    'signal':{
+        '$or':[
+            {'text':re.compile('squawk',re.IGNORECASE)},
+            {'text':re.compile('code',re.IGNORECASE)},
+            {'text':re.compile('alarm',re.IGNORECASE)},
+            {'text':re.compile('signal',re.IGNORECASE)},
+            {'text':re.compile('button',re.IGNORECASE)},
+            {'text':re.compile('transponder',re.IGNORECASE)},
+            {'text':re.compile('7500')}
+        ]
+    },
+    'hijacking':{
+        '$or':[
+            {'text':re.compile('squawk',re.IGNORECASE)},
+            {'text':re.compile('signal',re.IGNORECASE)},
+            {'text':re.compile('button',re.IGNORECASE)},
+            {'text':re.compile('plane',re.IGNORECASE)},
+            {'text':re.compile('pilot',re.IGNORECASE)},
+            {'text':re.compile('transponder',re.IGNORECASE)},
+            {'text':re.compile('west',re.IGNORECASE)},
+            {'text':re.compile('flight',re.IGNORECASE)},
+            {'text':re.compile('jet',re.IGNORECASE)},
+            {'text':re.compile('7500')},
+        ]
+    },
+   ### MH17 RUMORS ##
+    'blackbox':{
+        '$and':[
+            {
+                '$or':[
+                    {'text':re.compile('rebels',re.IGNORECASE)},
+                    {'text':re.compile('separatists',re.IGNORECASE)},
+                    {'text':re.compile('terrorists',re.IGNORECASE)}
+                ]
+            },
+            {
+                '$or':[
+                    {'text':re.compile('blackbox',re.IGNORECASE)},
+                    {'text':re.compile('black box',re.IGNORECASE)},
+                    {'text':re.compile('recorder',re.IGNORECASE)}
+                ]
+            }
+        ]
+    },
+	# EBOLA RUMORS ##
+    'red_cross':{
+        '$and':[
+            {'text':re.compile('vaccine',re.IGNORECASE)},
+            {
+                '$or':[
+                    {'text':re.compile('red cross|redcross',re.IGNORECASE)},
+                    {'text':re.compile(' ARC ',re.IGNORECASE)},
+                    {'text':re.compile(' IRC ',re.IGNORECASE)}
+                ]
+            }
+        ]
+    },
+    'semen':{
+        '$or':[
+            {'text':re.compile('semen',re.IGNORECASE)},
+            {'text':re.compile('breast milk',re.IGNORECASE)},
+            {'text':re.compile('breastmilk,re.IGNORECASE')}
+        ]
+    },
+    'frontier':{
+        '$and':[
+            {'text':re.compile('frontier',re.IGNORECASE)},
+            {
+                '$or':[
+                    {'text':re.compile('ban',re.IGNORECASE)},
+                    {'text':re.compile('grounded',re.IGNORECASE)}
+                ]
+            }
+        ]
+    },
+    'quarantine':
+    {
+        '$or':[
+            {'text':re.compile('camps',re.IGNORECASE)},
+            {'text':re.compile('trailers',re.IGNORECASE)}
+        ]
+    },
+    ## SYDNEY SIEGE RUMORS ##
+    'gunmen':
+    {
+        '$or':[
+            {
+                '$and':[
+                    {'text':re.compile('gunman',re.IGNORECASE)},
+                    {
+                        '$or':[
+                            {'text':re.compile('single',re.IGNORECASE)},
+                            {'text':re.compile('one',re.IGNORECASE)},
+                            {'text':re.compile(' 1 ',re.IGNORECASE)},
+                            {'text':re.compile('only',re.IGNORECASE)},
+                        ]
+                    }
+                ]
+            },
+            {
+                '$and':[
+                    {'text':re.compile('gunmen',re.IGNORECASE)},
+                    {
+                        '$or':[
+                            {'text':re.compile(' 2',re.IGNORECASE)},
+                            {'text':re.compile('2 ',re.IGNORECASE)},
+                            {'text':re.compile(' 3',re.IGNORECASE)},
+                            {'text':re.compile('3 ',re.IGNORECASE)},
+                            {'text':re.compile('two',re.IGNORECASE)},
+                            {'text':re.compile('three',re.IGNORECASE)},
+                            {'text':re.compile('multiple',re.IGNORECASE)},
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    # unused
+    'isis':
+    {
+        '$or':[
+            {'text':re.compile(' isis',re.IGNORECASE)},
+            {'text':re.compile('#isis',re.IGNORECASE)},
+        ]
+    },
+    'suicide':
+    {
+        '$or':[
+            {'text':re.compile('suicide',re.IGNORECASE)},
+            {'text':re.compile(' belt',re.IGNORECASE)},
+            {'text':re.compile(' vest',re.IGNORECASE)},
+            {'text':re.compile('backpack',re.IGNORECASE)},
+        ]
+    },
+    'airspace':
+    {
+        '$or':[
+            {'text':re.compile('airspace',re.IGNORECASE)},
+            {'text':re.compile('air space',re.IGNORECASE)},
+            {'text':re.compile('flights',re.IGNORECASE)},
+            {'text':re.compile('no-fly',re.IGNORECASE)},
+            {'text':re.compile('no fly',re.IGNORECASE)}
+        ]
+    },
+    # unused
+    'tweet':
+    {
+        '$and':[
+            {'text':re.compile('police',re.IGNORECASE)},
+            {
+                '$or':[
+                    {'text':re.compile('ask',re.IGNORECASE)},
+                    {'text':re.compile('request',re.IGNORECASE)}
+                ]
+            },
+            {
+                '$or':[
+                    {'text':re.compile('tweet',re.IGNORECASE)},
+                    {'text':re.compile('post',re.IGNORECASE)},
+                    {'text':re.compile('social media',re.IGNORECASE)},
+                    {'text':re.compile('share',re.IGNORECASE)}
+                ]
+            }
+        ]
+    },
+    # unused
+    'priest':
+    {
+        'text':re.compile('priest',re.IGNORECASE)
+    },
+    'hadley':
+    {
+        '$and':[
+            {'text':re.compile('hostage',re.IGNORECASE)},
+            {
+                '$or':[
+                    {'text':re.compile('hadley',re.IGNORECASE)},
+                    {'text':re.compile('radio host',re.IGNORECASE)}
+                ]
+            }
+        ]
+    },
+    'lakemba':
+    {
+        '$and':[
+            {'text':re.compile('lakemba',re.IGNORECASE)},
+            {'text':re.compile('^((?!vigil).)*$',re.IGNORECASE)},
+        ]
+    },
+    'flag':
+    {
+        '$and':[
+            {'text':re.compile('flag',re.IGNORECASE)},
+            {
+                '$or':[
+                    {'text':re.compile(' isis',re.IGNORECASE)},
+                    {'text':re.compile('#isis',re.IGNORECASE)},
+                    {'text':re.compile('isil',re.IGNORECASE)}
+                ]
+            }
+        ]
+    },
+    'americans_onboard':
+    {
+        '$and':[
+            {
+                '$or':[
+                    {'text':re.compile('passenger',re.IGNORECASE)},
+                    {'text':re.compile('board',re.IGNORECASE)},
+                    {'text':re.compile('23',re.IGNORECASE)},
+                ]
+            },
+            {
+                '$or':[
+                    {'text':re.compile('americans',re.IGNORECASE)},
+                    {'text':re.compile('us citizen',re.IGNORECASE)},
+                ]
+            }
+        ]
+    },
+    'rebels':
+    {
+        '$and':[
+            {'text':re.compile('ukrain',re.IGNORECASE)},
+            {'text':re.compile('shot',re.IGNORECASE)},
+            {
+                '$or':[
+                    {'text':re.compile(' rebel',re.IGNORECASE)},
+                    {'text':re.compile('separatist',re.IGNORECASE)}
+                ]
+            }
+        ]
+    },
+    'american_falseflag':
+    {
+        '$and':[
+            {
+                '$or':[
+                    {'text':re.compile('falseflag',re.IGNORECASE)},
+                    {'text':re.compile('false flag',re.IGNORECASE)},
+                ]
+            },
+            {
+                '$or':[
+                    {'text':re.compile('america',re.IGNORECASE)},
+                    {'text':re.compile('usa',re.IGNORECASE)},
+                ]
+            }
+        ]
+    },
+    'israel_falseflag':
+    {
+        '$and':[
+            {
+                '$or':[
+                    {'text':re.compile('falseflag',re.IGNORECASE)},
+                    {'text':re.compile('false flag',re.IGNORECASE)},
+                ]
+            },
+            {
+                '$or':[
+                    {'text':re.compile('israel',re.IGNORECASE)},
+                    {'text':re.compile('zion',re.IGNORECASE)},
+                ]
+            }
+        ]
+    },
+    'same_plane':
+    {
+        '$and':[
+            {'text':re.compile('mh17',re.IGNORECASE)},
+            {'text':re.compile('mh370',re.IGNORECASE)},
+            {'text':re.compile('same',re.IGNORECASE)},
+        ]
+    },
+    'blackbox':{
+        '$and':[
+            {
+                '$or':[
+                    {'text':re.compile('rebels',re.IGNORECASE)},
+                    {'text':re.compile('separatists',re.IGNORECASE)},
+                    {'text':re.compile('terrorists',re.IGNORECASE)}
+                ]
+            },
+            {
+                '$or':[
+                    {'text':re.compile('blackbox',re.IGNORECASE)},
+                    {'text':re.compile('black box',re.IGNORECASE)},
+                    {'text':re.compile('recorder',re.IGNORECASE)}
+                ]
+            }
+        ]
+    },
+}
+
+event_rumor_map = {
+    'sydneysiege':['lakemba','suicide','hadley','flag','airspace'],
+    'mh17':['american_falseflag','americans_onboard'],
+    'westjet':['hijacking'],
+    'dc_power_outage':['foulplay'],
+    'donetsk':['nuclear_detonation'],
+    'baltimore':['purse','church_fire','gangs']
+}
+
+filter_words = {
+    'hadley':['hostage','hadley','radio','host'],
+    'lakemba':['lakemba'],
+    'flag':['flag','isis','isil'],
+    'suicide':['suicide','belt','vest','backpack'],
+    'airspace':['airspace','air','space','flights','no','fly','no-fly'],
+    'americans_onboard':['passenger','board','23','americans','us','citizen'],
+    'hijacking':['squawk','signal','button','plane','pilot','transponder','west','flight','jet','7500'],
+    #'church_fire':[]
+}
+
+event_terms = {
+    'sydneysiege':['sydneysiege','martinplacesiege','haron','monis','haronmonis','illridewithyou','martinplace','sydney','chocolate shop','nswpolice','prime minister','tony abbott','witness','lindt','siege','hostage','hostages','martin place','terrorise','terrorize','terrorists','flag'],
+    'mh17':['sa11','sa-11','ukraine','malaysia','airlines','mh17','kiev','mh017','torez','donetsk','airline','plane','buk'],
+    'WestJet_Hijacking':[],
+    'baltimore':[]
+}
