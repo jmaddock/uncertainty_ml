14b79d2725fd651c829425526851e961211c6985
jmaddock <skimaddster@gmail.com>
2015-09-02 08:30:03 -0700

    added mongo insertion to tweet processor script

--- scripts/process_tweets.py
+++ scripts/process_tweets.py
@@ -4,6 +4,9 @@ from pandas import DataFrame
 import rumor_terms
 import numpy
 import os
+import pickle
+import re
+import json
 
 # db info
 client = MongoClient('z') #fix this
@@ -45,16 +48,18 @@ def process_tweet(tweet,event=None,rumor=None):
         cleaned += word + ' '
     return cleaned
 
-def process_and_pickle(event_list,fname=None,verbose=False):
+def process_and_pickle(event_list,fname,insert=False,verbose=False):
     for event in event_list:
         count = 0
         result = DataFrame({'text':[],'event':[],'features':[]})
+        if insert:
+            insert_list = []
         if verbose:
             print 'processing data from %s' % (event)
         examples = client[event]['tweets'].find()
         for tweet in examples:
-            if count == 10:
-                break
+            if verbose and count % 1000 == 0:
+                print 'processed %s tweets' % count
             if tweet['text']:
                 #full_tweet = get_tweet_meta_data(tweet,event,rumor)
                 features = {}
@@ -72,20 +77,34 @@ def process_and_pickle(event_list,fname=None,verbose=False):
                     'event':event,
                     'features':json.dumps(features)
                 },index=[count]))
+                if insert:
+                    insert_list.append({
+                        'id':tweet['id'],
+                        'text':text,
+                        'event':event,
+                        'features':features
+                    })
+                    if count % 1000 == 0:
+                        client['uncertainty_ml'][event].insert(insert_list)
+                        insert_list = []
                 count += 1
+        if insert:
+            client['uncertainty_ml'][event].insert(insert_list)
+            insert_list = []
         result = result.reindex(numpy.random.permutation(result.index))
 
-        if fname:
-            fpath = os.path.join(os.path.dirname(__file__),os.pardir,'dicts/') + event + '_' + fname
-            f = open(fpath, 'w')
-            pickle.dump(result,f)
-            f.close()
+        fpath = os.path.join(os.path.dirname(__file__),os.pardir,'dicts/') + event + '_' + fname
+        f = open(fpath, 'w')
+        pickle.dump(result,f)
+        f.close()
         if verbose:
             print result
 
 def main():
-    process_and_pickle(event_list=['sydney_siege'],
+    process_and_pickle(event_list=['sydneysiege','new_boston','mh17'],
                        fname='to_classify_9-02.pickle',
+                       insert=True,
+                       verbose=True
     )
 
 if __name__ == "__main__":
